376bd493bf74807fdcb9d70178bf72dba4257ec5
Arthur Fernandez <arthur@snooget.com>
2023-06-05 08:21:13 -0700

    added react routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+  <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/> 
+        <Route path="/profile" element={<Profile/>}/>      
+        <Route path="/sign-in" element={<SignIn/>}/>      
+        <Route path="/sign-up" element={<SignUp/>}/>      
+        <Route path="/forogt-password" element={<ForgotPassword/>}/>      
+        <Route path="/offers" element={<Offers/>}/>      
+     
+      </Routes>
+    </Router>
+  </>
   );
 }
 
