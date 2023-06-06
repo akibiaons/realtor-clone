9e8da1db1d174a2a3a18b9d8bdfa05959e740eb4
Arthur Fernandez <arthur@snooget.com>
2023-06-05 20:30:07 -0700

    added useNavigate functionality to the header with react-router-dom

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const location = useLocation(); // To the left is init..
+  const navigate = useNavigate(); // Same here <--
+  function pathMathRoute(route){
+    if(route === location.pathname) {
+      return true;
+    }
+  };
   return (
-    <div>
-      <header>
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
         <div>
-          <img src="" alt="realtor logo" />
+          <img src="https://raw.githubusercontent.com/akibiaons/realtor-clone/main/public/logo.png" alt="realtor logo" className='h-10 cursor-pointer'
+          onClick={() => navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && "text-black border-b-red-500"}`} 
+            onClick={()=>navigate("/")}>Home</li>
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && "text-black border-b-red-500"}`}
+            onClick={()=>navigate("/offers")}
+            >Offers</li>
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`}
+            onClick={()=>navigate("/sign-in")}
+            >Sign In</li>
+          </ul> 
         </div>
       </header>
     </div>
